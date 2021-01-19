818f7a0dfb1a6cc21dd45bb89e59e8c93cbf6027
raimbaev223 <raimbaev.223@gmail.com>
2021-01-19 21:33:36 +0600

    commit

---
+++ account/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from .forms import LoginForm, UserRegistrationForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Вход выполнен успешно')
+            else:
+                return HttpResponse('Учетная запись не активна')
+        else:
+            return HttpResponse('Нет такого пользователя или неправильный пароль')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
